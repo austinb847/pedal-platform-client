a05e792b7d5c1cc74b2e9827ffe12b402eb82489
austinb847 <austinb847@icloud.com>
2020-07-27 09:53:05 -0700

    add react-bootstrap, display pedal names under each image

--- src/components/Pedals.js
+++ src/components/Pedals.js
@@ -2,13 +2,14 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as a from "./../actions";
 import { Link } from "react-router-dom";
-// import Image from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
 
 function Pedals(props) {
   const { error, isLoading, pedals } = props.apiResponse;
   const { dispatch } = props;
 
-  useEffect((props) => {
+  useEffect(() => {
     dispatch(a.makePedalApiCall('http://localhost:3001/api/v1/pedals'));
     return () => {};
   }, []);
@@ -21,13 +22,21 @@ function Pedals(props) {
   } else {
     return (
       <React.Fragment>
-        {pedals.map((pedal, index) => (
-
-          <img id={index} width="250px" src={pedal.image_url} alt="pedal"/>
-        ))}
-        <button onClick= {() => dispatch(a.makePedalApiCall('http://localhost:3001/api/v1/overdrive_pedals'))}></button>
+        <Container>
+          <div className="row">
+            {pedals.map((pedal, index) => (
+              <div className="column">
+                <div className="pedal-item">
+                  <Image id={index} src={pedal.image_url} width="250px" alt="pedal" thumbnail/>
+                  <div className="pedal-name">{pedal.name}</div>
+                </div>
+              </div>
+            ))}
+            <button onClick= {() => dispatch(a.makePedalApiCall('http://localhost:3001/api/v1/overdrive_pedals'))}></button>
+          </div>
+        </Container>
       </React.Fragment>
-    )
+    );
   }
 }
 
