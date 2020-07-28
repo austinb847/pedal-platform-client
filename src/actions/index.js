9eae1488653da0ba937adc4c134761e2952b9955
austinb847 <austinb847@icloud.com>
2020-07-28 13:59:01 -0700

    create api call action for getting a pedal by id

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,6 +15,21 @@ export const makePedalApiCall = (url) => {
   }
 }
 
+export const getSelectedPedal = (id) => {
+  return dispatch => {
+    dispatch(requestSelectedPedal);
+    return fetch(`http://localhost:3001/api/v1/${id}`)
+    .then(response => response.json())
+    .then(
+      (jsonifiedResponse) => {
+        dispatch(getSelectedPedalSuccess(jsonifiedResponse));
+      })
+      .catch((error) => {
+        dispatch(getSelectedPedalFailure(error));
+      });
+  }
+}
+
 export const requestPedals = () => ({
   type: c.REQUEST_PEDALS
 });
@@ -26,5 +41,20 @@ export const getPedalsSuccess = (pedals) => ({
 
 export const getPedalsFailure = (error) => ({
   type: c.GET_PEDALS_FAILURE,
+  error
+});
+
+
+export const requestSelectedPedal = () => ({
+  type: c.REQUEST_PEDAL_BY_ID
+});
+
+export const getSelectedPedalSuccess = (pedal) => ({
+  type: c.GET_SELECTED_PEDAL_SUCCESS,
+  pedal
+});
+
+export const getSelectedPedalFailure = (error) => ({
+  type: c.GET_SELECTED_PEDAL_FAILURE,
   error
 });
