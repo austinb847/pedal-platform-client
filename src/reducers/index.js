80ddc482504ac447c108351f4808b311a9f5ec5a
austinb847 <austinb847@icloud.com>
2020-07-28 14:05:33 -0700

    add selected-pedal-reducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import apiReducer from './api-reducer';
+import selectedPedalReducer from './selected-pedal-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-  getPedalsResponse: apiReducer
+  getPedalsResponse: apiReducer,
+  getSelectedPedal: selectedPedalReducer
 });
 
 export default rootReducer;
