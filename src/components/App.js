6eba797528f73990c76de094bf4d38b08499a798
austinb847 <austinb847@icloud.com>
2020-07-29 16:04:54 -0700

    add new inventory route, pass adminStatus as a prop to nav component

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,8 @@ import Navigation from './Navigation'
 import Login from './registrations/Login'
 import Signup from './registrations/Signup'
 import Pedals from './Pedals'
-import PedalDetail from './PedalDetail';
+import PedalDetail from './PedalDetail'
+import Inventory from './Inventory'
 
 class App extends Component {
   constructor(props) {
@@ -31,34 +32,26 @@ class App extends Component {
     })
     .catch(error => console.log('api errors:', error))
   }
-  
+
   handleLogin = (data) => {
-    if(data.user.admin) {
-      this.setState({
-        isLoggedIn: true,
-        isAdmin: true,
-        user: data.user
-      })
-    } else {
-      this.setState({
-        isLoggedIn: true,
-        isAdmin: false,
-        user: data.user
-      })
-    }
+    this.setState({
+      isLoggedIn: true,
+      user: data.user,
+      isAdmin: data.user.admin ? true : false
+    })
   }
+
   handleLogout = () => {
       this.setState({
       isLoggedIn: false,
-      user: {},
-      isAdmin: false
-      })
+      user: {}
+    })
   }
 render() {
     return (
       <div>
         <BrowserRouter>
-          <Navigation handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn}/>
+          <Navigation handleLogout={this.handleLogout} loggedInStatus={this.state.isLoggedIn} adminStatus={this.state.isAdmin}/>
           <Switch>
             <Route 
               exact path='/' 
@@ -88,6 +81,12 @@ render() {
               exact path='/pedals/:id'
               render={props => (
                 <PedalDetail {...props} />
+              )}
+            />
+             <Route
+              exact path='/inventory'
+              render={props => (
+                <Inventory {...props} />
               )}
             />
           </Switch>
