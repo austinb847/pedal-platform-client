94cc76a15ebb7e0a55b83238cf667ff703c656d4
austinb847 <austinb847@icloud.com>
2020-07-29 16:06:49 -0700

    wrap about text in div container

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import Navigation from './Navigation'
 import instructionsImg from '../instructions.png'
-import phoneImg from '../7.png'
 import ImageFader from './ImageFader'
 import Button from 'react-bootstrap/Button'
 
 function Home(props) {
   return (
     <React.Fragment>
-      {/* <Navigation {...props} handleLogout={props.handleLogout} loggedInStatus={props.loggedInStatus}/> */}
       <section class="intro">
         <div class="content">
           <ImageFader/>
@@ -26,8 +23,10 @@ function Home(props) {
       <section className="home-section">  
         <div class="about-content">
           <h1>About Pedal Platform</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin aliquet pulvinar. Aliquam erat volutpat. Praesent lobortis vulputate justo, ut maximus ipsum euismod nec. Donec eget imperdiet sapien, id dapibus libero. Quisque quis tristique orci. Aliquam erat volutpat. Aliquam vestibulum libero molestie dolor ultrices dapibus. Donec vitae pharetra purus.
-          Nam maximus elit ex, in tristique diam interdum id. Sed non semper nisl. Aliquam efficitur, purus non vehicula accumsan, metus enim ullamcorper nisl, nec rutrum felis nunc nec augue. Donec feugiat lobortis mauris. Quisque varius libero nec pellentesque sagittis. Duis at erat arcu. Fusce ac luctus nulla, et auctor arcu. Cras tristique odio sed tellus mattis, at semper ex facilisis.</p>
+          <div class="about-text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin aliquet pulvinar. Aliquam erat volutpat. Praesent lobortis vulputate justo, ut maximus ipsum euismod nec. Donec eget imperdiet sapien, id dapibus libero. Quisque quis tristique orci. Aliquam erat volutpat. Aliquam vestibulum libero molestie dolor ultrices dapibus. Donec vitae pharetra purus.
+            Nam maximus elit ex, in tristique diam interdum id. Sed non semper nisl. Aliquam efficitur, purus non vehicula accumsan, metus enim ullamcorper nisl, nec rutrum felis nunc nec augue. Donec feugiat lobortis mauris. Quisque varius libero nec pellentesque sagittis. Duis at erat arcu. Fusce ac luctus nulla, et auctor arcu. Cras tristique odio sed tellus mattis, at semper ex facilisis.</p>
+          </div>
         </div>
       </section>
  
@@ -38,7 +37,7 @@ function Home(props) {
       </section>
 
       <footer className="footer">
-        Site by <a href="">@Austin Butler</a>
+        Site by <a href="#">@Austin Butler</a>
       </footer>
     </React.Fragment>
   )
