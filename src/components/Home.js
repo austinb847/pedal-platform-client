ba5d55acd5dd2309dfdbeffa1c78a704d054f6b9
austinb847 <austinb847@icloud.com>
2020-07-29 15:06:55 -0700

    add about section to home page, update footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navigation from './Navigation'
 import instructionsImg from '../instructions.png'
+import phoneImg from '../7.png'
 import ImageFader from './ImageFader'
 import Button from 'react-bootstrap/Button'
 
@@ -18,25 +19,26 @@ function Home(props) {
 
       <section className="home-section">  
         <div class="content">
-          <h1>How Pedal Platform works:</h1>
-          <img styles={{textAlign: "center"}}src={instructionsImg}></img>
+          <img className="how-to-image" src={instructionsImg}></img>
         </div>
       </section>
 
       <section className="home-section">  
-        <div class="content">
-          <h1>It's amazing and fast.</h1>
+        <div class="about-content">
+          <h1>About Pedal Platform</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sollicitudin aliquet pulvinar. Aliquam erat volutpat. Praesent lobortis vulputate justo, ut maximus ipsum euismod nec. Donec eget imperdiet sapien, id dapibus libero. Quisque quis tristique orci. Aliquam erat volutpat. Aliquam vestibulum libero molestie dolor ultrices dapibus. Donec vitae pharetra purus.
+          Nam maximus elit ex, in tristique diam interdum id. Sed non semper nisl. Aliquam efficitur, purus non vehicula accumsan, metus enim ullamcorper nisl, nec rutrum felis nunc nec augue. Donec feugiat lobortis mauris. Quisque varius libero nec pellentesque sagittis. Duis at erat arcu. Fusce ac luctus nulla, et auctor arcu. Cras tristique odio sed tellus mattis, at semper ex facilisis.</p>
         </div>
       </section>
  
       <section className="home-section">  
         <div class="content">
-          <h1>See the <a href="http://caniuse.com/#feat=viewport-units">browser support.</a></h1>
+          <h1>Contact us <a href="#">contact@pedalplatform.com</a></h1>
         </div>
       </section>
 
       <footer className="footer">
-        Made by <a href="https://www.twitter.com/ckor">@ckor</a>
+        Site by <a href="">@Austin Butler</a>
       </footer>
     </React.Fragment>
   )
